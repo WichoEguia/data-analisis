34291b00de2ff179108c7bcc28213586a3382ff5
WichoEguia <jlewicho@gmail.com>
2020-05-20 01:30:41 -0500

    Agregando nuevo dataset para complementar el ya existente y cambios en metodos

--- linear_regression.py
+++ linear_regression.py
@@ -5,19 +5,18 @@ from sklearn import linear_model, metrics
 from sklearn.model_selection import train_test_split
 
 def linear_regression():
-  dataset = pd.read_csv('winequality-red.csv')
+  dataset_wine_red = pd.read_csv('winequality-red.csv')
+  dataset_wine_white = pd.read_csv('winequality-white.csv')
+  dataset = pd.concat([dataset_wine_red, dataset_wine_white])
+
   dataset.isnull().any() # Limpiando dataset
 
   X = dataset.iloc[:, :-1]
   Y = dataset.iloc[:, -1].values # Obteniendo propiedad quality
 
-  print('\nEntradas del dataset')
-  print('X = ', X.values)
-  print('Y = ', Y)
-
   # Entrenando X y Y
   # Dividiendo conjunto de prueba y entrenamiento
-  X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 42)
+  X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.7, random_state=5)
 
   # Usando los conjuntos de entrenamiento para entrenar el modelo
   reg = linear_model.LinearRegression()
@@ -35,8 +34,9 @@ def linear_regression():
   print('\nDiferencia entre datos de entrenamiento y los predichos (error)')
   print(df)
 
-  print('\Varianza: {}'.format(reg.score(X_test, Y_test)))
+  print('\nError absoluto:', metrics.mean_absolute_error(Y_test, y_test_pred))  
+  print('Error cuadrado:', metrics.mean_squared_error(Y_test, y_test_pred))  
+  print('Error cuadratico:', np.sqrt(metrics.mean_squared_error(Y_test, y_test_pred)))
 
-  print('\nError absoluto:', metrics.mean_absolute_error(Y_test, y_pred))  
-  print('Error cuadrado:', metrics.mean_squared_error(Y_test, y_pred))  
-  print('Error cuadratico:', np.sqrt(metrics.mean_squared_error(Y_test, y_pred)))
+  accuracy = reg.score(X_test, Y_test)
+  print("Presición: {}%".format(int(round(accuracy * 100))))
--- machine_learning.py
+++ machine_learning.py
@@ -1,37 +1,45 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn import metrics
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
 def machine_learning():
-  dataset = pd.read_csv('winequality-red.csv')
+  dataset_wine_red = pd.read_csv('winequality-red.csv')
+  dataset_wine_white = pd.read_csv('winequality-white.csv')
+  dataset = pd.concat([dataset_wine_red, dataset_wine_white])
+
   dataset.isnull().any() # Limpiando dataset
 
   print('\nValores del campo quality:')
   print(dataset['quality'].value_counts()) # Los valores quality tienen un rango de 2 a 8
 
   # Clasificando la calidad del vino [quality] en malo y bueno según el rango
-  bins = (2, 6.5, 8)
-  group_names = ['malo', 'bueno']
-  dataset['quality'] = pd.cut(dataset['quality'], bins = bins, labels = group_names)
+  # reviews = []
+  # for i in dataset['quality']:
+  #     if i >= 1 and i <= 3:
+  #         reviews.append('1')
+  #     elif i >= 4 and i <= 7:
+  #         reviews.append('2')
+  #     elif i >= 8 and i <= 10:
+  #         reviews.append('3')
+  # dataset['quality'] = reviews
 
   # Conviertiendo categorias "buena" y "mala" en valores numericos
-  # -> 0 = malo
-  # -> 1 = bueno
-  label_quality = LabelEncoder()
-  dataset['quality'] = label_quality.fit_transform(dataset['quality'])
-  dataset['quality'].value_counts()
+  # -> 1 = malo
+  # -> 2 = medio
+  # -> 3 = bueno
+  # label_quality = LabelEncoder()
+  # dataset['quality'] = label_quality.fit_transform(dataset['quality'])
 
   X = dataset.iloc[:, :-1]
   Y = dataset.iloc[:, -1].values # Obteniendo propiedad quality
 
   # Entrenando X y Y
   # Dividiendo conjunto de prueba y entrenamiento
-  X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+  X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
   # Optimizando el resultado con escalado estandar
   sc = StandardScaler()
@@ -44,4 +52,4 @@ def machine_learning():
   pred_svc = svc.predict(X_test)
 
   print()
-  print(classification_report(Y_test, pred_svc))
+  print(metrics.classification_report(Y_test, pred_svc))
